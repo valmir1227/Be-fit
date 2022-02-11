e4509b8e1e7979bd5c345bc31bc634aa87d568b2
joaovitor644 <joaorasp666@yahoo.com>
2022-02-10 21:29:05 -0300

    Adiçao de script de loger generater

---
+++ src/tools/Logger.ts
@@ -0,0 +1,36 @@
+const Logger = class Logger {
+    date = new Date(Date.now())
+
+    getDateLog(){
+        return `${this.date.getFullYear()}-${this.date.getMonth() + 1 < 10? "0" + `${this.date.getMonth() + 1}` : this.date.getMonth() + 1}-${this.date.getDay() < 10? "0" + `${this.date.getDay()}`: this.date.getDay()}`;
+    }
+    getClockLog(){
+        return `${this.date.getHours()}:${this.date.getMinutes()}:${this.date.getSeconds() < 10? "0" + `${this.date.getSeconds()}`: this.date.getSeconds() }`
+    }
+    info(message) {
+        return `${this.getDateLog()} ${this.getClockLog()} INFO ${message} \r\n`
+    }
+    debug(message) {
+        return `${this.getDateLog()} ${this.getClockLog()} DEBUG ${message} \r\n`
+    }
+    warning(message) {
+        return `${this.getDateLog()} ${this.getClockLog()} WARNING ${message} \r\n`
+    }
+    error(message) {
+        return `${this.getDateLog()} ${this.getClockLog()} ERROR ${message} \r\n`
+    }
+    critical(message) {
+        return `${this.getDateLog()} ${this.getClockLog()} CRITICAL ${message} \r\n`
+    }
+}
+
+const createLog = function createLog(msg,pathToFile){
+    const fs = require('fs')
+
+    fs.writeFile(pathToFile ,msg,{encoding:'utf-8',flag:"a+"},() => {});
+}
+
+module.exports = {
+    createLog,
+    Logger
+}
